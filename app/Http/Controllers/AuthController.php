d5d24af9047c77eb8b7eafb248ec84b20cba8b21
ajay <ajay.garg579@gmail.com>
2024-07-10 23:04:56 +0530

    update

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -127,7 +127,7 @@ class AuthController extends Controller
 
     public function webhook(Request $request)
     {
-        $emails = SuccessfulEmail::get();
+        $emails = SuccessfulEmail::where('raw_text','')->latest(100)->get();
         foreach ($emails as $email) {
             try {
                $parse = Helper::parseRawEmail($email->email);
